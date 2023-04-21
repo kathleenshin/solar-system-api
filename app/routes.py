6bac016c6389209f1dfcb7a9cc6aa81acc46bdaf
Selene Castillo <secast@live.com>
2023-04-21 11:31:16 -0700

    added validate planet and handle_planet

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet():
     def __init__(self, id, name, description, x_parameter):
@@ -22,3 +22,26 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 def handle_planets():
     planets_as_dict = [vars(planet) for planet in planets]
     return jsonify(planets_as_dict), 200
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+
+    abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "x_parameter": planet.x_parameter
+    }
