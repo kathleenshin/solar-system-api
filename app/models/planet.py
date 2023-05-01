fb649876e9a6f67ed385d3e6a343851f6840f27f
kathleen <kathleen.n.shin@gmail.com>
2023-05-01 14:35:17 -0700

    Define planet helper method to_dict

--- app/models/planet.py
+++ app/models/planet.py
@@ -5,3 +5,11 @@ class Planet(db.Model):
     name = db.Column(db.String)
     description = db.Column(db.String)
     has_rings = db.Column(db.Boolean)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "has_rings": self.has_rings
+        }
