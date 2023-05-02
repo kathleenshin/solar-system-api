17b790d6b13313f84e4fc975518d8f967c60d17d
kathleen <kathleen.n.shin@gmail.com>
2023-05-02 12:04:45 -0700

    Created .env file, updated create_app to switch between testing and development database

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    if test_config is None:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DEV_DATABASE_URI')
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('TEST_DATABASE_URI')
+    
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres@localhost:5432/solar_system_development'
 
     from app.models.planet import Planet
 
