eddba48dca28a4b45c6822a617a302988e818055
kathleen <kathleen.n.shin@gmail.com>
2023-05-05 14:36:08 -0700

    Create from_dict class instance and update to_dict to planet_as_dict

--- app/models/planet.py
+++ app/models/planet.py
@@ -6,10 +6,28 @@ class Planet(db.Model):
     description = db.Column(db.String)
     has_rings = db.Column(db.Boolean)
 
+    # def to_dict(self):
+    #     return {
+    #         "id": self.id,
+    #         "name": self.name,
+    #         "description": self.description,
+    #         "has_rings": self.has_rings
+    #     }
+
     def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "has_rings": self.has_rings
-        }
+        planet_as_dict = {}
+        planet_as_dict["id"] = self.id
+        planet_as_dict["name"] = self.name
+        planet_as_dict["description"] = self.description
+        planet_as_dict["has_rings"] = self.has_rings
+
+        return planet_as_dict
+    
+    @classmethod
+    def from_dict(cls, planet_data):
+        new_planet = Planet(name=planet_data["name"],
+                            description=planet_data["description"],
+                            has_rings=planet_data["has_rings"])
+        return new_planet
+
+
